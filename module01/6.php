ace5922925fc441693033084134a01d9f491ba82
James <9022370@student.zadkine.nl>
2024-09-27 13:49:21 +0200

    opdarcth 6 af

--- module01/6.php
+++ module01/6.php
@@ -6,13 +6,15 @@ class Product
     public $price;
     public $category;
     public $description;
+    public $rating;
 
-    public function __construct($name, $price, $category, $description)
+    public function __construct($name, $price, $category = "dranken", $description, $rating = 3)
     {
         $this->name = strtolower($name);
         $this->price = number_format($this->price*1.09, 2);
-        $this->category = strtoupper($this->category);
+        $this->category = strtoupper($category);
         $this->description = $description;
+        $this->rating = $rating;
     }
     
     public function setCategory()
@@ -21,14 +23,14 @@ class Product
     }
 };
 
-$A = new Product("Toilet Tower defence", 1.00, "Tower defence", "blah blah bleh blhe za zip ziep ozp zao");
+$A = new Product("Toilet Tower defence", 1.00, "Tower defence", "blah blah bleh blhe za zip ziep ozp zao", -1);
 
-$B = new Product("Moonshire", 19.99, "RPG", "Open world rpg, with a story line and cool workd");
+$B = new Product("Moonshire", 19.99, "RPG", "Open world rpg, with a story line and cool workd", rating:100);
 
-$C = new Product("Elden Ring", 59.99, "RPG", "peak gaming");
+$C = new Product("Elden Ring", 59.99, "RPG", "peak gaming", 11);
 
 echo "<pre>";
-echo $A->name."<br>".$A->price."<br>".$A->category."<br>".$A->description."<br>";
+echo $A->name."<br>".$A->price."<br>".$A->category."<br>".$A->description."<br>".$A->rating."<br><br>";
 var_dump($A, $B, $C);
 echo "</pre>";
 ?>
